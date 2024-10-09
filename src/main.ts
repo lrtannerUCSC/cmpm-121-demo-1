2793c290053fd63ee8c2f982d4ea03592adc2221
tickyyyy <102706661+tickyyyy@users.noreply.github.com>
2024-10-09 11:17:41 -0700

    Step 1: Created a Button

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,18 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const oilButton = document.createElement("button");
+oilButton.textContent = "🏗️";
+
+oilButton.style.backgroundColor = "#ffcc00"; // Light Orange
+oilButton.style.border = "none";
+oilButton.style.color = "white";
+oilButton.style.padding = "10px 20px";
+oilButton.style.fontSize = "16px";
+oilButton.style.cursor = "pointer";
+
+oilButton.addEventListener("click", () => {
+})
+
+document.body.appendChild(oilButton);
